4faa069947ca21e6a25c622c710fba37cb4efdcb
John Albert <john.m.albert@gmail.com>
2025-01-27 15:29:56 -0800

    updating appInsights

--- src/appInsights.js
+++ src/appInsights.js
@@ -2,6 +2,10 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 const connectionString = process.env.REACT_APP_APPINSIGHTS_CONNECTION_STRING;
 
+if (!connectionString) {
+    throw new Error('Please provide instrumentation key');
+}
+
 const appInsights = new ApplicationInsights({
   config: {
     connectionString: connectionString
